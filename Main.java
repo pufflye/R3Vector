ef48672ccf0f0743f0557ad60463d21ffebbdc58
pufflye <56954184+pufflye@users.noreply.github.com>
2019-12-26 21:13:17 +0300

    Update and rename README.md to Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+# R3Vector //задача на вектор
+public class Main
+{
+    public static void main(String[] args)
+    {
+        R3Vector f = new R3Vector();
+        R3Vector e = new R3Vector();
+        R3Vector l = new R3Vector();
+
+        R3Vector.sum(f, e);
+        R3Vector.raz(f, e);
+        R3Vector.scalar(f, e);
+        R3Vector.vsum(f, e);
+        R3Vector.sm(f, e, l);
+        R3Vector.vch(f);
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# R3Vector
-R3Vector
